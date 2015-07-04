ac9d9338149cf33d73bd9b3997c98f9caf0a504b
Paul Sideleau <paul.sideleau@surveysampling.com>
2015-07-04 12:57:09 -0400

    finish diagonal differences submission

--- hackathon/diagonaldifference/src/diagonaldifference/core.clj
+++ hackathon/diagonaldifference/src/diagonaldifference/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str])
   (:import java.lang.Math))
 
+
 (defn recur-down-diagonal  [matrix, sumvector, index]
   (cond
     (= index (count matrix)) sumvector
@@ -11,13 +12,9 @@
   (recur-down-diagonal matrix [] 0))
 
 (defn recur-up-diagonal  [matrix, sumvector, x-index, y-index]
-  (do
-    (println sumvector)
-    (println x-index)
-    (println y-index)
   (cond
     (= y-index (count matrix)) sumvector
-    :else (recur matrix (conj sumvector (get-in matrix [x-index y-index])) (- x-index 1) (+ 1 y-index)))))
+    :else (recur matrix (conj sumvector (get-in matrix [x-index y-index])) (- x-index 1) (+ 1 y-index))))
 
 (defn get-up-diagonal  [matrix]
   (recur-up-diagonal matrix [] (- (count matrix) 1) 0))
@@ -25,7 +22,14 @@
 (defn sum-diagonals [matrix]
   (. Math (abs (- (reduce + (get-down-diagonal  matrix)) (reduce + (get-up-diagonal  matrix))))))
 
+(defn split-string [vector-string]
+  (mapv #(Integer/parseInt %) (str/split vector-string #" ")))
+
+(defn build-matrix [matrix, size, counter]
+  (if (>= counter size) (do (println matrix) matrix)
+    (recur (conj matrix (split-string (read-line))) size (inc counter))))
+
 
 (defn -main []
-  (let [mycount (read-line) mylist (read-line)]
-    (println (diagonaldifference.core/sum-diagonals mylist))))
+  (let [mycount (Integer/parseInt (read-line)) ]
+     (println (sum-diagonals (build-matrix [] mycount 0)))))
